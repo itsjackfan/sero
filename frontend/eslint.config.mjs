506fc899f4fe4701bf5cb18e070bf8367b5e15db
JACK FAN <jack.fan.dev@gmail.com>
2025-09-30 10:35:10 -0400

    disable eslint for now

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,7 +10,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +18,7 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {},
   },
 ];
 
